b4cb0089e9985b7dc86d122a8f028289c50b143a
Andrey Safonov <andrey.safonov@gmail.com>
2024-03-05 04:25:24 +0300

    - add MCCMNC to the log - add MCC to the regional_fallback

--- src/main.c
+++ src/main.c
@@ -7,19 +7,15 @@
 #include "xlog.h"
 
 #define TARGET_ID "it_iliad"
+//#define TARGET_ID "testsim"
+
 #define ID_F (1 << 0)
 #define HASH_F (1 << 1)
-
-/*
-#define RU_MCC 250
-#define UA_MCC 255
-#define BY_MCC 257
-#define KZ_MCC 401
-*/
 
 typedef struct mccmnc {
 	list_head list;
 	char id[16];
+	char mccmnc[16];
 	char name[256];
 } mccmnc;
 
@@ -28,13 +24,14 @@ static const char tgtReq[] = "SELECT DISTINCT confnames.carrier_id, confmap.conf
 							 "WHERE confnames.name = '%s' "
 							 "AND confmap.carrier_id = confnames.carrier_id";
 
-static const char mccReq[] = "SELECT DISTINCT carrier_info.carrier_id, carrier_name.name "
+static const char mccReq[] = "SELECT DISTINCT carrier_info.carrier_id, carrier_name.name, carrier_info.mccmnc "
 							 "FROM carrier_info, carrier_name "
-							 "WHERE carrier_info.mccmnc LIKE '%s%%' "
+							 "WHERE substr(carrier_info.mccmnc, 1, 3) = '%s'"
 							 "AND carrier_info.carrier_id = carrier_name.carrier_id";
 
-static const char updReq[] = "INSERT OR IGNORE INTO confmap VALUES('%s', NULL, '%s')";
-static const char upd0Req[] = "UPDATE confmap SET confman = '%s' WHERE carrier_id = '0'";
+static const char updReq[] = "INSERT OR REPLACE INTO confmap VALUES('%s', NULL, '%s')";
+
+static const char updDefMcc[] = "INSERT OR REPLACE INTO regional_fallback VALUES('%s', '%s')";
 
 static char tgtId[16];
 static char tgtHash[64];
@@ -92,16 +89,18 @@ static int getTgt(sqlite3 *db)
 
 static int mccCb(void *data, int argc, char **argv, char **azColName)
 {
-	if (argc != 2)
+	if (argc != 3)
 		return -1;
 	mccmnc *v;
 	list_for_each_entry(v, &mccList, list)
 		if (strcmp(argv[0], v->id) == 0)
 			return 0;
-	mccmnc *m = calloc(1, sizeof(*m));
+	mccmnc *m = malloc(sizeof(*m));
+	memset(m, 0, sizeof(*m));
 	INIT_LIST_HEAD(&m->list);
 	strncpy(m->id, argv[0], sizeof(m->id) - 1);
 	strncpy(m->name, argv[1], sizeof(m->name) - 1);
+	strncpy(m->mccmnc, argv[2], sizeof(m->mccmnc) - 1);
 	list_add_tail(&m->list, &mccList);
 	return 0;
 }
@@ -128,12 +127,12 @@ static int putMcc(sqlite3 *db, const char *id)
 	return 0;
 }
 
-static int upd0Mcc(sqlite3 *db)
+static int putDefMcc(sqlite3 *db, const char *id)
 {
 	char sql[512];
-	sprintf(sql, upd0Req, tgtHash);
+	sprintf(sql, updDefMcc, id, tgtId);
 	if (sqlite3_exec(db, sql, NULL, NULL, NULL) != SQLITE_OK) {
-		errorf("putMcc(0): %s\n", sqlite3_errmsg(db));
+		errorf("putDefMcc(%s): %s\n", id, sqlite3_errmsg(db));
 		return -1;
 	}
 	return 0;
@@ -155,19 +154,22 @@ int main(int argc, char **argv)
 		goto out;
 	infof("target name '%s', id %s, hash '%s'\n", TARGET_ID, tgtId, tgtHash);
 	int i;
-	for (i = 2; i < argc; i++)
+	for (i = 2; i < argc; i++) {
 		if (getMcc(db, argv[i]))
 			break;
+		if (putDefMcc(db, argv[i]))
+			break;
+	}
 	if (i < argc)
 		goto out;
 	mccmnc *v;
 	list_for_each_entry(v, &mccList, list) {
-		infof("update record id %s, operator '%s'\n", v->id, v->name);
+		infof("update record id %s, mccmnc '%s', operator '%s'\n", v->id, v->mccmnc, v->name);
 		if (putMcc(db, v->id))
 			goto out;
 	}
 	infof("update default record id 0\n");
-	if (upd0Mcc(db))
+	if (putMcc(db, "0"))
 		goto out;
 	rv = EXIT_SUCCESS;
 out:
