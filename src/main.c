ac7b91810dbd9e7e5f77065cc3ac49b4558553b8
Andrey Safonov <andrey.safonov@gmail.com>
2024-03-08 05:40:33 +0300

    remove regional_fallback due to high battery usage

--- src/main.c
+++ src/main.c
@@ -6,8 +6,9 @@
 #include "xlist.h"
 #include "xlog.h"
 
-#define TARGET_ID "it_iliad"
-//#define TARGET_ID "testsim"
+#define DB_IDX 1
+#define TGT_IDX 2
+#define MCC_START 3
 
 #define ID_F (1 << 0)
 #define HASH_F (1 << 1)
@@ -31,7 +32,11 @@ static const char mccReq[] = "SELECT DISTINCT carrier_info.carrier_id, carrier_n
 
 static const char updReq[] = "INSERT OR REPLACE INTO confmap VALUES('%s', NULL, '%s')";
 
+#if 0
 static const char updDefMcc[] = "INSERT OR REPLACE INTO regional_fallback VALUES('%s', '%s')";
+#endif
+
+static const char delDefMcc[] = "DELETE FROM regional_fallback WHERE country_code = '%s'";
 
 static char tgtId[16];
 static char tgtHash[64];
@@ -72,17 +77,17 @@ static int tgtCb(void *data, int argc, char **argv, char **azColName)
 	return 0;
 }
 
-static int getTgt(sqlite3 *db)
+static int getTgt(sqlite3 *db, const char *tgt_id)
 {
 	char sql[512];
-	sprintf(sql, tgtReq, TARGET_ID);
+	sprintf(sql, tgtReq, tgt_id);
 	int cnt = 0;
 	if (sqlite3_exec(db, sql, tgtCb, &cnt, NULL) != SQLITE_OK) {
-		errorf("getTgt: %s\n", sqlite3_errmsg(db));
+		errorf("getTarget(%s): %s\n", tgt_id, sqlite3_errmsg(db));
 		return -1;
 	}
 	if (cnt != (ID_F | HASH_F)) {
-		errorf("id or hash not found for %s\n", TARGET_ID);
+		errorf("id or hash not found for %s\n", tgt_id);
 	}
 	return 0;
 }
@@ -127,12 +132,16 @@ static int putMcc(sqlite3 *db, const char *id)
 	return 0;
 }
 
-static int putDefMcc(sqlite3 *db, const char *id)
+static int updateDefMcc(sqlite3 *db, const char *id)
 {
 	char sql[512];
+#if 0
 	sprintf(sql, updDefMcc, id, tgtId);
+#else
+	sprintf(sql, delDefMcc, id);
+#endif
 	if (sqlite3_exec(db, sql, NULL, NULL, NULL) != SQLITE_OK) {
-		errorf("putDefMcc(%s): %s\n", id, sqlite3_errmsg(db));
+		errorf("updateDefMcc(%s): %s\n", id, sqlite3_errmsg(db));
 		return -1;
 	}
 	return 0;
@@ -140,44 +149,45 @@ static int putDefMcc(sqlite3 *db, const char *id)
 
 int main(int argc, char **argv)
 {
-	if (argc < 2)
-		exitf("usage: %s db_file [mcc list]\n", argv[0]);
+	if (argc < MCC_START)
+		exitf("usage: %s db_file tgt_name [mcc list]\n", argv[0]);
 	if (access(argv[1], R_OK | W_OK))
-		exitf("file %s: %m\n", argv[1]);
-	if (argc == 2)
+		exitf("file %s: %m\n", argv[DB_IDX]);
+	if (argc == MCC_START)
 		return 0;
 	int rv = EXIT_FAILURE;
-	sqlite3 *db = dbOpen(argv[1]);
+	sqlite3 *db = dbOpen(argv[DB_IDX]);
 	if (!db)
-		return rv;
-	if (getTgt(db))
-		goto out;
-	infof("target name '%s', id %s, hash '%s'\n", TARGET_ID, tgtId, tgtHash);
+		goto err0;
+	if (getTgt(db, argv[TGT_IDX]))
+		goto err1;
+	infof("target name '%s', id %s, hash '%s'\n", argv[TGT_IDX], tgtId, tgtHash);
 	int i;
-	for (i = 2; i < argc; i++) {
+	for (i = MCC_START; i < argc; i++) {
 		if (getMcc(db, argv[i]))
 			break;
-		if (putDefMcc(db, argv[i]))
+		if (updateDefMcc(db, argv[i]))
 			break;
 	}
 	if (i < argc)
-		goto out;
+		goto err1;
 	mccmnc *v;
 	list_for_each_entry(v, &mccList, list) {
 		infof("update record id %s, mccmnc '%s', operator '%s'\n", v->id, v->mccmnc, v->name);
 		if (putMcc(db, v->id))
-			goto out;
+			goto err1;
 	}
 	infof("update default record id 0\n");
 	if (putMcc(db, "0"))
-		goto out;
+		goto err1;
 	rv = EXIT_SUCCESS;
-out:
+err1:
 	dbClose(db);
 	mccmnc *n;
 	list_for_each_entry_safe(v, n, &mccList, list) {
 		list_del_init(&v->list);
 		free(v);
 	}
+err0:
 	return rv;
 }
--- src/xlog.h
+++ src/xlog.h
@@ -4,16 +4,18 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 
+#define LOGOUT stdout
+
 #define infof(fmt, ...)                      \
 	do {                                     \
-		fprintf(stdout, fmt, ##__VA_ARGS__); \
-		fflush(stdout);                      \
+		fprintf(LOGOUT, fmt, ##__VA_ARGS__); \
+		fflush(LOGOUT);                      \
 	} while (0)
 
 #define errorf(fmt, ...)                     \
 	do {                                     \
-		fprintf(stderr, fmt, ##__VA_ARGS__); \
-		fflush(stderr);                      \
+		fprintf(LOGOUT, fmt, ##__VA_ARGS__); \
+		fflush(LOGOUT);                      \
 	} while (0)
 
 #define exitf(fmt, ...)             \
@@ -31,48 +33,11 @@
 #ifdef DEBUG
 #define debugf(fmt, ...)                     \
 	do {                                     \
-		fprintf(stdout, fmt, ##__VA_ARGS__); \
-		fflush(stdout);                      \
+		fprintf(LOGOUT, fmt, ##__VA_ARGS__); \
+		fflush(LOGOUT);                      \
 	} while (0)
 #else
 #define debugf(fmt, ...) \
 	do {                 \
 	} while (0)
 #endif
-
-#define die()                                                            \
-	do {                                                                 \
-		errorf("died at %s:%d[%s]\n", __FILE__, __LINE__, __FUNCTION__); \
-		abort();                                                         \
-	} while (0)
-
-#define dief(fmt, ...)              \
-	do {                            \
-		errorf(fmt, ##__VA_ARGS__); \
-		die();                      \
-	} while (0)
-
-static inline char *prM(char *buf, const void *p)
-{
-	const uint8_t *m = p;
-	sprintf(buf, "%02x:%02x:%02x:%02x:%02x:%02x", m[0], m[1], m[2], m[3], m[4], m[5]);
-	return buf;
-}
-
-static inline char *prIp4(char *buf, const void *p)
-{
-	inet_ntop(AF_INET, p, buf, INET_ADDRSTRLEN);
-	return buf;
-}
-
-static inline char *prIp6(char *buf, const void *p)
-{
-	inet_ntop(AF_INET6, p, buf, INET6_ADDRSTRLEN);
-	return buf;
-}
-
-static inline char *prIp(char *buf, int af, const void *p)
-{
-	inet_ntop(af, p, buf, af == AF_INET ? INET_ADDRSTRLEN : INET6_ADDRSTRLEN);
-	return buf;
-}
